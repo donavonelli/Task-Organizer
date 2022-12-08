518f93cc79f3c283cea1873eb58711e594e6e6ec
Donavon Ellison <dev@Donavons-MBP.attlocal.net>
2022-12-08 10:37:42 -0500

    Added body component and finished list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { FinishedListComponent } from './finished-list/finished-list.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     TodoListComponent,
-    FinishedListComponent
+    FinishedListComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
